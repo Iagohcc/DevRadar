00fe169e35a110d166a11f8ddf084d514e3ed223
Iago Henrique <iago.hcc@gmail.com>
2020-03-03 22:12:13 -0300

    alteração

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+<div id="app">
+  <aside>
+<strong>Cadastrar</strong>
+  <form>
+    <div class="input-block">
+    <label htmlFor="gthub_username">Usuário do Github</label>
+    <input name="github_username" id="username_github" require/>
     </div>
+
+    <div class="input-block">
+    <label htmlFor="techs">Técnologias</label>
+    <input name="techs" id="techs" require/>
+    </div>
+
+    <div className="input-group">
+    <div class="input-block">
+    <label htmlFor="latitude">Latitude</label>
+    <input name="latitude" id="latitude" require/>
+    </div>
+
+    <div class="input-block">
+    <label htmlFor="longitude">Longitude</label>
+    <input name="longitude" id="longitude" require/>
+    </div>
+    </div>
+<button type="submit">Salvar</button>
+  </form>
+  </aside>
+  <main>
+
+  </main>
+</div>
   );
 }
 
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+//Toda vez q tiver um html(tag) tem q importar o react
+
 import ReactDOM from 'react-dom';
-import './index.css';
+//React DOM comunicação
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//Renderizando o app no root
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//JSX (JavaScript + HTML)
